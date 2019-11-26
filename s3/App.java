0b8d8e4c965e251254be08f898998ffcaa168419
Sandeepkumarkuppili <42691003+Sandeepkumarkuppili@users.noreply.github.com>
2019-11-26 18:49:37 +0530

    Update App.java

--- s3/App.java
+++ s3/App.java
@@ -1,4 +1,17 @@
 
+import java.util.Scanner;
+
+
 public class App {
+	 
+	
+	public static void main( String[] args )
+	    {
+	        DataService dataService = new DataService();
+	        System.out.println( "Number of terms : " + dataService.CreateConnection());
+	        Scanner sc=new Scanner(System.in);
+	        String term=sc.nextLine();
+	        dataService.saveTerm(term);
+	    }
 
 }
